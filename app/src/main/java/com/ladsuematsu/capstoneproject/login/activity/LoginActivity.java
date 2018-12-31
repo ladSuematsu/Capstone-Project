d3ad0f3993cb3fa868e23b306279525eff38add9
Cristhian Suematsu <suematsu01@gmail.com>
2018-12-30 23:21:01 -0200

    Use Borderless, Colored Material style Buttons

--- app/src/main/java/com/ladsuematsu/capstoneproject/login/activity/LoginActivity.java
+++ app/src/main/java/com/ladsuematsu/capstoneproject/login/activity/LoginActivity.java
@@ -1,37 +1,21 @@
 package com.ladsuematsu.capstoneproject.login.activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.ladsuematsu.capstoneproject.overview.MapActivity;
 import com.ladsuematsu.capstoneproject.R;
@@ -39,8 +23,6 @@ import com.ladsuematsu.capstoneproject.login.mvp.LoginMvp;
 import com.ladsuematsu.capstoneproject.login.mvp.model.LoginModel;
 import com.ladsuematsu.capstoneproject.login.mvp.presenter.LoginPresenter;
 import com.ladsuematsu.capstoneproject.util.UiUtils;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -151,7 +133,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -159,7 +141,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button emailSignUpButton = findViewById(R.id.email_sign_up_button);
+        Button emailSignUpButton = findViewById(R.id.email_sign_up);
         emailSignUpButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
